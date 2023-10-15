d8f6dfd1389aa53c1b26ce030f665315bd4eee6d
Munupeace <peacevwegbamunu@gmail.com>
2023-10-15 10:56:43 -0700

    corrected putchar

--- _putchar.c
+++ _putchar.c
@@ -3,7 +3,9 @@
 /**
 * _putchar - custom function to print a character,
 * interger or percentage to the standard output (stdout)
-* Return: on success, 1. on error, -1
+* This function takes a character 'c' and writes it to the standard output
+* @c: The character to be printed.
+* Return: on success, 1. on error, -1, and errno is set appropriately.
 */
 int _putchar(char c)
 {
