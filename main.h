b7c60614a10f17d088d37cf67c7781af62fd0cd1
Munupeace <peacevwegbamunu@gmail.com>
2023-10-16 05:37:51 -0700

    task1

--- main.h
+++ main.h
@@ -8,6 +8,7 @@
 int _print_string(char *str);
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_decimal(va_list args);
-int print_integer(va_list args);
+int print_dec(va_list args);
+int print_int(va_list args);
+int print_unsigned(va_list args);
 #endif
